edfa985b363c6dc848e1aeddbcf8493b80b02a0c
Mauiboo <132040849+Mauiboo@users.noreply.github.com>
2025-05-15 19:43:23 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://frontend-zr2n.onrender.com", // ← important!
+  })
+);
+
+
 app.use(express.json({ limit: "4mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
