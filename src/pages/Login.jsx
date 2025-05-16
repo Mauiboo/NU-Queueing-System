342d7e1021b9ae51d3ab26576b37c9ab8974c09d
Mauiboo <132040849+Mauiboo@users.noreply.github.com>
2025-05-16 16:41:59 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://backend-ryp4.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
